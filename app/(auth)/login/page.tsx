2f577acbae610f4b9a09ead8d59509626b5ccb15
arekhalpern <arek.halpern@gmail.com>
2025-03-11 10:44:54 -0400

    typo fixed;

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -63,7 +63,7 @@ export default function Page() {
           </div>
           <h1 className="text-2xl font-light">YC Founder&apos;s Chat</h1>
           <p className="text-sm text-justify text-muted-foreground">
-            Explore and learn about Y-Combinator founders through an intelligent
+            Explore and learn about Y Combinator founders through an intelligent
             chat interface. Our database contains information on ~7,500
             YC-funded founders enriched with LinkedIn data including education,
             skills, and work experience.
--- components/overview.tsx
+++ components/overview.tsx
@@ -13,7 +13,7 @@ export const Overview = () => {
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
         <h1 className="text-2xl font-light">YC Founder&apos;s Chat</h1>
         <p className="text-justify text-muted-foreground">
-          Explore and learn about Y-Combinator founders through an intelligent
+          Explore and learn about Y Combinator founders through an intelligent
           chat interface. Our database contains information on ~7,500 YC-funded
           founders enriched with LinkedIn data including education, skills, and
           work experience.
